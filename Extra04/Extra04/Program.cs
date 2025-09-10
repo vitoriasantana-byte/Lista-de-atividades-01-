34b320b9ec1db3a437b3d5b42230f848494a1076
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:31:15 -0300

    Add files via upload

---
+++ Extra04/Extra04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o preço do produto: R$ ");
+        double preco = double.Parse(Console.ReadLine());
+
+        double desconto = preco * 0.05;
+        double precoFinal = preco - desconto;
+
+        Console.WriteLine("Preço com 5% de desconto: R$ " + precoFinal.ToString("F2"));
+    }
+}
