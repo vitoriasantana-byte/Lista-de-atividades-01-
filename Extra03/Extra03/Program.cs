2b34d21cba6e7b9125b7fc3668893fceffa5ca08
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:29:21 -0300

    Add files via upload

---
+++ Extra03/Extra03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Susing System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de A: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de B: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de C: ");
+        double c = double.Parse(Console.ReadLine());
+
+        double delta = (b * b) - (4 * a * c);
+
+        Console.WriteLine("O valor de Delta é: " + delta);
+    }
+}
