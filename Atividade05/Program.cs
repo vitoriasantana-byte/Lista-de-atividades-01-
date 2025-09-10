b5bc588dcdbae7dbef41c54090f653a29b20087d
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:11:14 -0300

    Add files via upload

---
+++ Atividade05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a idade em anos: ");
+        int anos = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a idade em meses: ");
+        int meses = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite os dias: ");
+        int dias = int.Parse(Console.ReadLine());
+
+        int totalDias = (anos * 365) + (meses * 30) + dias;
+
+        Console.WriteLine("A idade em dias é: " + totalDias);
+    }
+}
