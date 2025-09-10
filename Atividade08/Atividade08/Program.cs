a0fe36e8ffad93e5ef5d9e74c0faa1ee72613d6d
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:19:53 -0300

    Add files via upload

---
+++ Atividade08/Atividade08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 10)
+        {
+            Console.WriteLine("É MAIOR QUE 10!");
+        }
+        else
+        {
+            Console.WriteLine("NÃO É MAIOR QUE 10!");
+        }
+    }
+}
