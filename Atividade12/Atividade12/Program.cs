8f9800c198aa17999ab8975d87c04ccd78b2ec25
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 09:31:20 -0300

    Add files via upload

---
+++ Atividade12/Atividade12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o ano atual: ");
+        int anoAtual = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o ano de nascimento: ");
+        int anoNascimento = int.Parse(Console.ReadLine());
+
+        int idade = anoAtual - anoNascimento;
+
+        if (idade < 16)
+        {
+            Console.WriteLine("Não vota.");
+        }
+        else if ((idade >= 18 && idade <= 70))
+        {
+            Console.WriteLine("O voto é obrigatório.");
+        }
+        else
+        {
+            Console.WriteLine("O voto é facultativo.");
+        }
+    }
+}
