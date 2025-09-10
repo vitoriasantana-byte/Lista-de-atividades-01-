7d023b0104f144b0e45983cd2dbcfdd347c727d0
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:31:10 -0300

    Add files via upload

---
+++ Atividade10/Atividade10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a quantidade de maçãs compradas: ");
+        int qtd = int.Parse(Console.ReadLine());
+
+        double preco;
+
+        if (qtd < 12)
+        {
+            preco = qtd * 1.30;
+        }
+        else
+        {
+            preco = qtd * 1.00;
+        }
+
+        Console.WriteLine("O valor total da compra é: R$ " + preco);
+    }
+}
