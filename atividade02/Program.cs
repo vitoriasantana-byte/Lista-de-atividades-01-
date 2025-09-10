a35beb90b5f0813264532b74227691814066688f
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 07:55:55 -0300

    Add files via upload

---
+++ atividade02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//atv1
+class Program
+{
+    static void Main()
+    {
+        int A = 10;
+        int B = 20;
+
+        Console.WriteLine("Antes da troca: A = " + A + ", B = " + B);
+
+        // Troca usando variável auxiliar
+        int aux = A;
+        A = B;
+        B = aux;
+
+        Console.WriteLine("Depois da troca: A = " + A + ", B = " + B);
+    }
+}
