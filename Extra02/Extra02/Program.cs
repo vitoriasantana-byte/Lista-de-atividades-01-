e72c35e63a4aade4e6befbc2e8b391ec5d44347c
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:12:37 -0300

    Add files via upload

---
+++ Extra02/Extra02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a largura da parede (em metros): ");
+        double largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura da parede (em metros): ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = largura * altura;
+        double tinta = area / 2.0; // cada litro pinta 2 m²
+
+        Console.WriteLine("Área da parede: " + area + " m²");
+        Console.WriteLine("Quantidade de tinta necessária: " + tinta + " litros");
+    }
+}
