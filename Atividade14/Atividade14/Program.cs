9b3eeacdc5b724c18e80245cd3af7a5a6d8cb388
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:08:56 -0300

    Add files via upload

---
+++ Atividade14/Atividade14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro valor: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo valor: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o terceiro valor: ");
+        int c = int.Parse(Console.ReadLine());
+
+        // Coloca os valores em um vetor e usa sort
+        int[] valores = { a, b, c };
+        Array.Sort(valores);
+
+        Console.WriteLine("Valores em ordem crescente: ");
+        foreach (int v in valores)
+        {
+            Console.WriteLine(v);
+        }
+    }
+}
