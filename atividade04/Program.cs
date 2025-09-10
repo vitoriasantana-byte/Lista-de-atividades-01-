e863164e945afb25c9e3ee357437090f8e51f19b
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:10:09 -0300

    Add files via upload

---
+++ atividade04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite a base do retângulo: ");
+        double baseRet = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura do retângulo: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double area = baseRet * altura;
+
+        Console.WriteLine("A área do retângulo é: " + area);
+    }
+}
