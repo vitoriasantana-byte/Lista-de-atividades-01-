7d21a77c409ee601442f2ba616abfd047fedf159
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:10:55 -0300

    Add files via upload

---
+++ Extra01/Extra01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite quanto dinheiro você tem em reais (R$): ");
+        double reais = double.Parse(Console.ReadLine());
+
+        double dolares = reais / 3.45;
+
+        Console.WriteLine("Você pode comprar: US$ " + dolares.ToString("F2"));
+    }
+}
