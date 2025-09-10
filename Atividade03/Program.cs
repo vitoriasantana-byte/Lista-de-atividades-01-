9b40ed2c0360d17873f0e8413e6b7e8e1d3b9096
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:09:30 -0300

    Add files via upload

---
+++ Atividade03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int antecessor = numero - 1;
+
+        Console.WriteLine("O antecessor de " + numero + " é " + antecessor);
+    }
+}
