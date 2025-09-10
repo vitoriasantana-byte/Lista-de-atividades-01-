783389fa3e12858b5872fbb49dc07cb8fe683272
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 08:24:34 -0300

    Add files via upload

---
+++ Atividade09/Atividade09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número (diferente de zero): ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("Valor zero não é permitido!");
+        }
+    }
+}
