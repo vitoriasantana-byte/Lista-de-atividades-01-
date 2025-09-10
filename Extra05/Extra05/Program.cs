e4fcecbfdfb01cd87e3947d25dc85a633d940391
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-10 10:35:03 -0300

    Add files via upload

---
+++ Extra05/Extra05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o salário atual do funcionário: R$ ");
+        double salario = double.Parse(Console.ReadLine());
+
+        double aumento = salario * 0.15;
+        double novoSalario = salario + aumento;
+
+        Console.WriteLine("Novo salário com 15% de aumento: R$ " + novoSalario.ToString("F2"));
+    }
+}
