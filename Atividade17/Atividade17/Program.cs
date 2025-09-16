e27c5cd8f1ada9d7a6da2b36bf5dbba3d613398e
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-16 09:02:00 -0300

    Add files via upload

---
+++ Atividade17/Atividade17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número inteiro para calcular o fatorial: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("Fatorial de número negativo não existe!");
+        }
+        else
+        {
+            long fatorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fatorial *= i;
+            }
+            Console.WriteLine($"{n}! = {fatorial}");
+        }
+    }
+}
