270c0724d92b0a469f61be3f019824bd2950f2c7
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-16 08:14:20 -0300

    Add files via upload

---
+++ Atividade16/Atividade16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números entre 1000 e 2000 com resto 5 na divisão por 11:");
+
+        for (int i = 1000; i <= 2000; i++)
+        {
+            if (i % 11 == 5)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
