014f85983146cd5e093c1b2a2fccb649809b6d6e
vitoriasantana-byte <vitoriasantanarossini@gmail.com>
2025-09-16 08:11:39 -0300

    Add files via upload

---
+++ Atividade15/Atividade15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um valor N (maior que zero): ");
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Exemplo(while,for)/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num, resultado, valor;
+
+        Console.WriteLine("Digite um número para a multiplicação:");
+        num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Até que valor irá a multiplicação?");
+        valor = int.Parse(Console.ReadLine());
+
+        int contador = 1;
+
+        while (contador <= valor)
+        {
+            resultado = contador * num;
+            Console.WriteLine($"{num} x {contador} = {resultado}");
+            contador++;
+        }
+    }
+}
